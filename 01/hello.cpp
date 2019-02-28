902ad0f5f9353352e147d3006598d2a4a59f0105
Anton Ly <anton96lu@gmail.com>
2019-02-28 22:45:30 +0100

    Fixed.

--- 01/hello.cpp
+++ 01/hello.cpp
@@ -41,6 +41,8 @@ std::pair<const char *, int> parse_args(int argc, char *argv[])
         {
             std::cerr << "error: 2nd argument must be an integral greater than zero!" << '\n';
         }
+        if (count < 0)
+            std::cerr << "error: 2nd argument must be an integral greater than zero!" << '\n';
         return std::make_pair(argv[1], count);
     }
 }
