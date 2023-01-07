d2ce4055d1a6819f178d66f70702ef9d747b4ef3
Anton Lu <anton96lu@gmail.com>
2023-01-07 21:59:13 +0100

    add real matrix class

--- 04-matrix/tests.cpp
+++ 04-matrix/tests.cpp
@@ -1,79 +1,79 @@
-#include <gtest/gtest.h>
+#include <catch2/catch.hpp>
 #include "matrix.hpp"
 #include <sstream>
 
-TEST(istream_test, 1) {
+TEST_CASE("istream test") {
   Matrix<double> matrix(2, 3);
 
   std::istringstream s("[[1 2 3]\n[4 5 6]]");
   s >> matrix;
 
-  EXPECT_EQ( matrix(0, 0) ,  1 );
-  EXPECT_EQ( matrix(0, 1) ,  2 );
-  EXPECT_EQ( matrix(0, 2) ,  3 );
-  EXPECT_EQ( matrix(1, 0) ,  4 );
-  EXPECT_EQ( matrix(1, 1) ,  5 );
-  EXPECT_EQ( matrix(1, 2) ,  6 );
+  CHECK( matrix(0, 0) == 1 );
+  CHECK( matrix(0, 1) == 2 );
+  CHECK( matrix(0, 2) == 3 );
+  CHECK( matrix(1, 0) == 4 );
+  CHECK( matrix(1, 1) == 5 );
+  CHECK( matrix(1, 2) == 6 );
 
-  EXPECT_EQ( matrix.rows() ,  2 );
-  EXPECT_EQ( matrix.columns() ,  3 );
+  CHECK( matrix.rows() == 2 );
+  CHECK( matrix.columns() == 3 );
 }
 
-TEST(constructors, 1) {
+TEST_CASE("Constructors") {
   /* Default constructor */
   Matrix<double> empty;
 
-  EXPECT_EQ( empty.rows() ,  0 );
-  EXPECT_EQ( empty.columns() ,  0 );
-  EXPECT_THROW( empty(0,0), std::out_of_range );
+  CHECK( empty.rows() == 0 );
+  CHECK( empty.columns() == 0 );
+  CHECK_THROWS_AS( empty(0,0), std::out_of_range );
 
   std::ostringstream out;
   out << empty;
-  EXPECT_EQ( out.str() ,  "[]" );
+  CHECK( out.str() == "[]" );
 
 
-  /* default constructor */
-  Matrix<double> sing_constr(2);
+  /* Implicit constructor */
+  Matrix<double> implicit(2);
 	 
-  EXPECT_EQ(sing_constr.rows() , 2 );
-  EXPECT_EQ(sing_constr.columns() , 2 );
-  EXPECT_EQ(sing_constr(0, 0) , 0 );
-  EXPECT_EQ(sing_constr(0, 1) , 0 );
-  EXPECT_EQ(sing_constr(1, 0) , 0 );
-  EXPECT_EQ(sing_constr(1, 1) , 0 );
-  EXPECT_THROW(sing_constr(2, 0), std::out_of_range );
-  EXPECT_THROW(sing_constr(0, 2), std::out_of_range );
+  CHECK( implicit.rows() == 2 );
+  CHECK( implicit.columns() == 2 );
+  CHECK( implicit(0,0) == 0 );
+  CHECK( implicit(0,1) == 0 );
+  CHECK( implicit(1,0) == 0 );
+  CHECK( implicit(1,1) == 0 );
+  CHECK_THROWS_AS( implicit(2, 0), std::out_of_range );
+  CHECK_THROWS_AS( implicit(0, 2), std::out_of_range );
 
   /* Explicit constructor */
   Matrix<double> expl(2, 3);
-  EXPECT_EQ( expl.rows() ,  2 );
-  EXPECT_EQ( expl.columns() ,  3 );
-  EXPECT_EQ( expl(0,0) ,  0 );
-  EXPECT_EQ( expl(0,1) ,  0 );
-  EXPECT_EQ( expl(0,2) ,  0 );
-  EXPECT_EQ( expl(1,0) ,  0 );
-  EXPECT_EQ( expl(1,1) ,  0 );
-  EXPECT_EQ( expl(1,2) ,  0 );
-  EXPECT_THROW( expl(2, 2), std::out_of_range );
-  EXPECT_THROW( expl(1, 3), std::out_of_range );
+  CHECK( expl.rows() == 2 );
+  CHECK( expl.columns() == 3 );
+  CHECK( expl(0,0) == 0 );
+  CHECK( expl(0,1) == 0 );
+  CHECK( expl(0,2) == 0 );
+  CHECK( expl(1,0) == 0 );
+  CHECK( expl(1,1) == 0 );
+  CHECK( expl(1,2) == 0 );
+  CHECK_THROWS_AS( expl(2, 2), std::out_of_range );
+  CHECK_THROWS_AS( expl(1, 3), std::out_of_range );
 
   /* Initializer list */
   Matrix<int> initialiser = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-  EXPECT_EQ( initialiser.rows() ,  3 );
-  EXPECT_EQ( initialiser.columns() ,  3 );
-  EXPECT_EQ( initialiser(0, 0) ,  1 );
-  EXPECT_EQ( initialiser(0, 1) ,  2 );
-  EXPECT_EQ( initialiser(0, 2) ,  3 );
-  EXPECT_EQ( initialiser(1, 0) ,  4 );
-  EXPECT_EQ( initialiser(1, 1) ,  5 );
-  EXPECT_EQ( initialiser(1, 2) ,  6 );
-  EXPECT_EQ( initialiser(2, 0) ,  7 );
-  EXPECT_EQ( initialiser(2, 1) ,  8 );
-  EXPECT_EQ( initialiser(2, 2) ,  9 );
+  CHECK( initialiser.rows() == 3 );
+  CHECK( initialiser.columns() == 3 );
+  CHECK( initialiser(0, 0) == 1 );
+  CHECK( initialiser(0, 1) == 2 );
+  CHECK( initialiser(0, 2) == 3 );
+  CHECK( initialiser(1, 0) == 4 );
+  CHECK( initialiser(1, 1) == 5 );
+  CHECK( initialiser(1, 2) == 6 );
+  CHECK( initialiser(2, 0) == 7 );
+  CHECK( initialiser(2, 1) == 8 );
+  CHECK( initialiser(2, 2) == 9 );
 
 }
 
-TEST(addition, 1) {
+TEST_CASE("Addition") {
   Matrix<double> one(2, 3);
   Matrix<double> two(2, 3);
   Matrix<double> three(2, 2);
@@ -89,16 +89,16 @@ TEST(addition, 1) {
   std::istringstream res_num("[[8 7 7]\n[12 10 9]]");
   res_num >> res;
 
-  EXPECT_EQ( sum ,  res );
+  CHECK( sum == res );
 
   one += two;
-  EXPECT_EQ( one ,  res );
+  CHECK( one == res );
 
-  EXPECT_THROW( one + three, std::out_of_range );
-  EXPECT_THROW( one += three, std::out_of_range );
+  CHECK_THROWS_AS( one + three, std::out_of_range );
+  CHECK_THROWS_AS( one += three, std::out_of_range );
 }
 
-TEST(scalar_addition, 1) {
+TEST_CASE("Scalar addition") {
   Matrix<double> one(2, 3);
 
   std::istringstream first("[[1 2 3]\n[4 5 6]]");
@@ -114,11 +114,11 @@ TEST(scalar_addition, 1) {
   rres_num >> rres;
   lres_num >> lres;
 
-  EXPECT_EQ( rsum ,  rres );
-  EXPECT_EQ( lsum ,  lres );
+  CHECK( rsum == rres );
+  CHECK( lsum == lres );
 }
 
-TEST(subtraction, 1) {
+TEST_CASE("Subtraction") {
   Matrix<double> one(2, 3);
   Matrix<double> two(2, 3);
 
@@ -133,13 +133,13 @@ TEST(subtraction, 1) {
   std::istringstream res_num("[[-6 -3 -1]\n[-4 0 3]]");
   res_num >> res;
 
-  EXPECT_EQ( diff ,  res );
+  CHECK( diff == res );
 
   one -= two;
-  EXPECT_EQ( one ,  res );
+  CHECK( one == res );
 }
 
-TEST(Scalar_subtraction, 1) {
+TEST_CASE("Scalar subtraction") {
   Matrix<double> one(2, 3);
 
   std::istringstream first("[[1 2 3]\n[4 5 6]]");
@@ -155,11 +155,11 @@ TEST(Scalar_subtraction, 1) {
   rres_num >> rres;
   lres_num >> lres;
 
-  EXPECT_EQ( rdiff ,  rres );
-  EXPECT_EQ( ldiff ,  lres );
+  CHECK( rdiff == rres );
+  CHECK( ldiff == lres );
 }
 
-TEST(Multiplication, 1) {
+TEST_CASE("Multiplication") {
   Matrix<double> one(2, 3);
   Matrix<double> two(3, 2);
   Matrix<double> three(2, 2);
@@ -175,17 +175,17 @@ TEST(Multiplication, 1) {
   std::istringstream res_num("[[30 30]\n[78 78]]");
   res_num >> res;
 
-  EXPECT_EQ( prod ,  res );
+  CHECK( prod == res );
 
   Matrix<double> one_orig = one;
   one *= two;
-  EXPECT_EQ( one ,  res );
+  CHECK( one == res );
 
-  EXPECT_THROW( one_orig * three, std::out_of_range );
-  EXPECT_THROW( one_orig *= three, std::out_of_range );
+  CHECK_THROWS_AS( one_orig * three, std::out_of_range );
+  CHECK_THROWS_AS( one_orig *= three, std::out_of_range );
 }
 
-TEST(scalar_multiplication, 1) {
+TEST_CASE("Scalar multiplication") {
   Matrix<double> one(2, 3);
 
   std::istringstream first("[[1 2 3]\n[4 5 6]]");
@@ -201,11 +201,11 @@ TEST(scalar_multiplication, 1) {
   rres_num >> rres;
   lres_num >> lres;
 
-  EXPECT_EQ( rprod ,  rres );
-  EXPECT_EQ( lprod ,  lres );
+  CHECK( rprod == rres );
+  CHECK( lprod == lres );
 }
 
-TEST(pointwise_multiplication, 1) {
+TEST_CASE("Pointwise multiplication") {
   Matrix<double> one(2, 3);
   Matrix<double> two(2, 3);
 
@@ -221,49 +221,49 @@ TEST(pointwise_multiplication, 1) {
   std::istringstream res_num("[[1 16 15][16 15 102]]");
   res_num >> res;
 
-  EXPECT_EQ( prod ,  res );
+  CHECK( prod == res );
 }
 
-TEST(stdout, 1) {
+TEST_CASE("stdout") {
   Matrix<int> mtx = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
   std::cout << mtx << std::endl;
 }
 
-TEST(access_operator, 1) {
+TEST_CASE("Access operator") {
   Matrix<int> mtx = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
   mtx(1, 2) = 10;
 
-  EXPECT_EQ( mtx(1, 2) ,  10 );
+  CHECK( mtx(1, 2) == 10 );
 }
 
-TEST(reset_operation, 1) {
+TEST_CASE("Reset operation") {
   Matrix<int> mtx = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
   mtx.reset();
 
-  EXPECT_EQ( mtx ,  Matrix<int>(3, 3) );
+  CHECK( mtx == Matrix<int>(3, 3) );
 }
   
-TEST(copy_operator, 1) {
+TEST_CASE("Copy operator") {
   Matrix<int> mtx = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
   Matrix<int> copy = mtx;
 
-  EXPECT_EQ( copy ,  mtx );
+  CHECK( copy == mtx );
 
   /* Make modifications in the copy */
   copy(1, 1) = 99;
-  EXPECT_NE( copy, mtx );
+  CHECK( copy != mtx );
 }
 
-TEST(move_operator, 1) {
+TEST_CASE("Move operator") {
   Matrix<int> mtx = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
   Matrix<int> move = std::move(mtx);
 
-  EXPECT_THROW( move == mtx, std::out_of_range );
+  CHECK_THROWS_AS( move == mtx, std::out_of_range );
 }
 
-TEST(insert_row, 1) {
+TEST_CASE("Insert row 1") {
   Matrix<double> test(2, 3);
   Matrix<double> res(3, 3);
 
@@ -275,10 +275,10 @@ TEST(insert_row, 1) {
 
   test.insert_row(1);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(insert_row, 2) {
+TEST_CASE("Insert row 2") {
   Matrix<double> test(2, 3);
   Matrix<double> res(3, 3);
 
@@ -290,10 +290,10 @@ TEST(insert_row, 2) {
 
   test.insert_row(0);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(append_row, 1) {
+TEST_CASE("Append row 1") {
   Matrix<double> test(2, 3);
   Matrix<double> res(3, 3);
 
@@ -305,10 +305,10 @@ TEST(append_row, 1) {
 
   test.append_row(0);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(append_row, 2) {
+TEST_CASE("Append row 2") {
   Matrix<double> test(2, 3);
   Matrix<double> res(3, 3);
 
@@ -320,10 +320,10 @@ TEST(append_row, 2) {
 
   test.append_row(1);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(remove_row, 1) {
+TEST_CASE("Remove row 1") {
   Matrix<double> test(2, 3);
   Matrix<double> res(1, 3);
 
@@ -335,10 +335,10 @@ TEST(remove_row, 1) {
 
   test.remove_row(1);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(remove_row, 2) {
+TEST_CASE("Remove row 2") {
   Matrix<double> test(2, 3);
   Matrix<double> res(1, 3);
 
@@ -350,10 +350,10 @@ TEST(remove_row, 2) {
 
   test.remove_row(0);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(insert_column, 1) {
+TEST_CASE("Insert column 1") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 4);
 
@@ -365,10 +365,10 @@ TEST(insert_column, 1) {
 
   test.insert_column(2);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(insert_column, 2) {
+TEST_CASE("Insert column 2") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 4);
 
@@ -380,10 +380,10 @@ TEST(insert_column, 2) {
 
   test.insert_column(0);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(append_column, 1) {
+TEST_CASE("Append column 1") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 4);
 
@@ -395,10 +395,10 @@ TEST(append_column, 1) {
 
   test.append_column(0);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(append_column, 2) {
+TEST_CASE("Append column 2") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 4);
 
@@ -410,10 +410,10 @@ TEST(append_column, 2) {
 
   test.append_column(2);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(remove_column, 1) {
+TEST_CASE("Remove column 1") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 2);
 
@@ -425,10 +425,10 @@ TEST(remove_column, 1) {
 
   test.remove_column(0);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(remove_column, 2) {
+TEST_CASE("Remove column 2") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 2);
 
@@ -440,10 +440,10 @@ TEST(remove_column, 2) {
 
   test.remove_column(2);
 
-  EXPECT_EQ( test ,  res );
+  CHECK( test == res );
 }
 
-TEST(get_column, 1) {
+TEST_CASE("Get column") {
   Matrix<double> test(2, 3);
   Matrix<double> res(2, 1);
 
@@ -455,10 +455,10 @@ TEST(get_column, 1) {
 
   Matrix<double> column = test.getColumn(0);
 
-  EXPECT_EQ( column ,  res );
+  CHECK( column == res );
 }
 
-TEST(get_row, 1) {
+TEST_CASE("Get row") {
   Matrix<double> test(2, 3);
   Matrix<double> res(1, 3);
 
@@ -470,18 +470,18 @@ TEST(get_row, 1) {
 
   Matrix<double> row = test.getRow(0);
 
-  EXPECT_EQ( row ,  res );
+  CHECK( row == res );
 }
 
-TEST(identity_matrix, 1) {
+TEST_CASE("Identity matrix") {
   Matrix<double> eye = Matrix<double>::eye(3);
 
   Matrix<double> reference = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };
 
-  EXPECT_EQ( eye ,  reference );
+  CHECK( eye == reference );
 }
 
-TEST(transpose, 1) {
+TEST_CASE("Transpose non-square") {
   Matrix<double> normal(2, 3);
   Matrix<double> transposed(3, 2);
 
@@ -491,12 +491,12 @@ TEST(transpose, 1) {
   std::istringstream end("[[1 4][2 5][3 6]]");
   end >> transposed;
 
-  EXPECT_EQ( normal.transpose() ,  transposed );
-  EXPECT_EQ( normal.rows() ,  3 );
-  EXPECT_EQ( normal.columns() ,  2 );
+  CHECK( normal.transpose() == transposed );
+  CHECK( normal.rows() == 3 );
+  CHECK( normal.columns() == 2 );
 }
 
-TEST(transpose_square, 1) {
+TEST_CASE("Transpose square") {
   Matrix<double> normal(3, 3);
   Matrix<double> transposed(3, 3);
 
@@ -506,12 +506,12 @@ TEST(transpose_square, 1) {
   std::istringstream end("[[1 4 7][2 5 8][3 6 9]]");
   end >> transposed;
 
-  EXPECT_EQ( normal.transpose() ,  transposed );
-  EXPECT_EQ( normal.rows() ,  3 );
-  EXPECT_EQ( normal.columns() ,  3 );
+  CHECK( normal.transpose() == transposed );
+  CHECK( normal.rows() == 3 );
+  CHECK( normal.columns() == 3 );
 }
 
-TEST(iterators, 1) {
+TEST_CASE("Iterators") {
   Matrix<double> normal(2, 3);
 
   std::istringstream s("[[1 2 3][4 5 6]]");
@@ -519,12 +519,9 @@ TEST(iterators, 1) {
 
   double i = 0;
   for (auto k = normal.begin(); k != normal.end(); k++)
-    EXPECT_EQ( *k ,  ++i );
+    CHECK( *k == ++i );
 
-  EXPECT_EQ( i ,  6.0 );
+  CHECK( i == 6.0 );
 }
 
-int main(int argc, char ** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+
